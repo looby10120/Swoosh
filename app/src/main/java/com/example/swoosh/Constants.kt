8e1900ca7b7aa42320a1f42c2910d789da0920ed
jedsadakorn.jiraphemounsarb <jedsadakorn.jiraphempunsarb@scb.co.th>
2020-05-15 18:18:03 +0700

    lesson 6

--- app/src/main/java/com/example/swoosh/Constants.kt
+++
@@ -1,4 +0,0 @@
-package com.example.swoosh
-
-const val EXTRA_LEAGUE = "league"
-const val TAG = "LifeCycle"
--- app/src/main/java/com/example/swoosh/Controller/BaseActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/BaseActivity.kt
@@ -1,4 +1,4 @@
-package com.example.swoosh
+package com.example.swoosh.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,37 +7,37 @@ import android.util.Log
 open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "${javaClass.simpleName} OnCreate")
+        Log.d(com.example.swoosh.Utilities.TAG, "${javaClass.simpleName} OnCreate")
         super.onCreate(savedInstanceState)
     }
 
     override fun onStart() {
-        Log.d(TAG, "${javaClass.simpleName} OnStart")
+        Log.d(com.example.swoosh.Utilities.TAG, "${javaClass.simpleName} OnStart")
         super.onStart()
     }
 
     override fun onResume() {
-        Log.d(TAG, "${javaClass.simpleName} onResume")
+        Log.d(com.example.swoosh.Utilities.TAG, "${javaClass.simpleName} onResume")
         super.onResume()
     }
 
     override fun onRestart() {
-        Log.d(TAG, "${javaClass.simpleName} onRestart")
+        Log.d(com.example.swoosh.Utilities.TAG, "${javaClass.simpleName} onRestart")
         super.onRestart()
     }
 
     override fun onPause() {
-        Log.d(TAG, "${javaClass.simpleName} onPause")
+        Log.d(com.example.swoosh.Utilities.TAG, "${javaClass.simpleName} onPause")
         super.onPause()
     }
 
     override fun onStop() {
-        Log.d(TAG, "${javaClass.simpleName} onStop")
+        Log.d(com.example.swoosh.Utilities.TAG, "${javaClass.simpleName} onStop")
         super.onStop()
     }
 
     override fun onDestroy() {
-        Log.d(TAG, "${javaClass.simpleName} onDestroy")
+        Log.d(com.example.swoosh.Utilities.TAG, "${javaClass.simpleName} onDestroy")
         super.onDestroy()
     }
 }
